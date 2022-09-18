acc5cdf704fedbdd813bc88354041ff2502c9843
loghomefinishing <74892898+loghomefinishing@users.noreply.github.com>
2022-09-18 12:13:29 -0600

    test

--- src/App.svelte
+++ src/App.svelte
@@ -25,6 +25,17 @@
   <p class="read-the-docs">
     Click on the Vite and Svelte logos to learn more
   </p>
+
+  <div class="card w-96 bg-base-100 shadow-xl">
+    <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+    <div class="card-body">
+      <h2 class="card-title">Shoes!</h2>
+      <p>If a dog chews shoes whose shoes does he choose?</p>
+      <div class="card-actions justify-end">
+        <button class="btn btn-primary">Buy Now</button>
+      </div>
+    </div>
+  </div>
 </main>
 
 <style>
